9babb3b60967f29e95c4de45756ae364929b5528
Matheus-Nasci <matheus.noliveira@sptech.school>
2023-02-08 21:40:46 -0300

    exercicio 3 interface e polimorfismo

---
+++ ed-exercicio-3/src/main/java/org/example/Servico.java
@@ -0,0 +1,30 @@
+package org.example;
+
+public class Servico implements Vendavel{
+    private String descricao;
+    private Integer codigo;
+    private Integer qtdHoras;
+    private Double valorHora;
+
+    public Servico(String descricao, Integer codigo, Integer qtdHoras, Double valorHora) {
+        this.descricao = descricao;
+        this.codigo = codigo;
+        this.qtdHoras = qtdHoras;
+        this.valorHora = valorHora;
+    }
+
+    @Override
+    public Double getValorVenda() {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "Servico{" +
+                "descricao='" + descricao + '\'' +
+                ", codigo=" + codigo +
+                ", qtdHoras=" + qtdHoras +
+                ", valorHora=" + valorHora +
+                '}';
+    }
+}
